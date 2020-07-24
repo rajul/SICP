947a8614d10f22e74225bffc2173ac68ebb1b5c2
Rajul <rajul09@gmail.com>
2020-07-24 12:12:14 +0800

    Added Follow Along Example 1.1.8: Procedures as Black-Box Abstractions

---
+++ Chapter1/example_1_1_8.scm
@@ -0,0 +1,19 @@
+(define (square x) (* x x))
+; (define (square x) (exp (double (log x))))
+; (define (double x) (+ x x))
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (sqrt x)
+  (define (improve guess)
+    (average guess (/ x guess)))
+
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+      guess
+      (sqrt-iter (improve guess))))
+
+  (sqrt-iter 1.0))
