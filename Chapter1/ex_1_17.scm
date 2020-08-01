3a1dd869d91194b3e268fd2eab2482c11449da72
Rajul <rajul09@gmail.com>
2020-08-01 22:48:26 +0800

    Added Exercise 1.17

---
+++ Chapter1/ex_1_17.scm
@@ -0,0 +1,18 @@
+(define (*-orig a b)
+  (if (= b 0) 0
+    (+ a (* a (- b 1)))))
+
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (*-new a b)
+  (cond ((= b 0) 0)
+	((= b 1) a)
+	((even? b) (double (*-new a (halve b))))
+	(else (+ a (*-new a (- b 1))))))
